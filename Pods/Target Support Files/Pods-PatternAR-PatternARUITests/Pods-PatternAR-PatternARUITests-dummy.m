fedd555eceb4f76be5c81a7db176013f910cceef
davalcato <davalcato@yahoo.com>
2020-06-12 15:03:20 -0700

    first commit

---
+++ Pods/Target Support Files/Pods-PatternAR-PatternARUITests/Pods-PatternAR-PatternARUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PatternAR_PatternARUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PatternAR_PatternARUITests
+@end
---
+++ Pods/Target Support Files/Pods-PatternAR/Pods-PatternAR-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PatternAR : NSObject
+@end
+@implementation PodsDummy_Pods_PatternAR
+@end
